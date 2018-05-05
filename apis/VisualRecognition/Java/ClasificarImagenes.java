5ba1e4ab47e8995ddc52f2e55389199552d7c2e3
Mirko Rodriguez <mirko.rodriguezm@gmail.com>
2018-05-04 22:23:38 -0500

    VR Java client calling

---
+++ apis/VisualRecognition/Java/ClasificarImagenes.java
@@ -0,0 +1,24 @@
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyImagesOptions;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.VisualClassification;
+
+public class ClasificarImagenes {
+	
+	public static void main(String[] args) {
+	
+		VisualRecognition service = new VisualRecognition(VisualRecognition.VERSION_DATE_2016_05_20);
+		service.setEndPoint("https://gateway-a.watsonplatform.net/visual-recognition/api");
+		service.setApiKey("your_api_key_here");
+		
+		String imageURL = new String("https://raw.githubusercontent.com/watson-developer-cloud/doc-tutorial-downloads/master/visual-recognition/fruitbowl.jpg");
+
+		ClassifyImagesOptions options = new ClassifyImagesOptions.Builder().url(imageURL).build();
+
+		VisualClassification result = service.classify(options).execute();
+
+		System.out.println("Classification Results:");
+
+		System.out.println(result);
+	
+	}
+}
